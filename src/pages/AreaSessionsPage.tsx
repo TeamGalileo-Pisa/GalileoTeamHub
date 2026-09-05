bee9ee09a6b78fa8551aa162527d3df38bee73f8
Alessio de Dato <107488474+Deda-404@users.noreply.github.com>
2026-09-05 13:24:38 +0200

    Integrate multi-day selector into Sessions page layout

--- src/pages/AreaSessionsPage.tsx
+++ src/pages/AreaSessionsPage.tsx
@@ -2,12 +2,5 @@ import { MultiDayAllocationPanel } from "../components/MultiDayAllocationPanel";
 import { SessionsPage } from "./SessionsPage";
 
 export function AreaSessionsPage() {
-  return (
-    <>
-      <div className="page-container">
-        <MultiDayAllocationPanel />
-      </div>
-      <SessionsPage />
-    </>
-  );
+  return <SessionsPage beforeCreate={<MultiDayAllocationPanel />} />;
 }
